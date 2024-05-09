ce6806a54d9efc34fbe30039f72cad282c1adad6
bluestorm <srijadutta0420@gmail.com>
2024-05-10 00:53:23 +0530

    initial commit

---
+++ src/logger_exception.py
@@ -0,0 +1,43 @@
+import sys
+import logging
+import datetime
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs")
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occurred in python script name [{file_name}] at line number [{exc_tb.tb_lineno}] error message[{str(error)}]"
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+
+
+# if __name__ == "__main__":
+
+#     try:
+    
+#         raise Exception("This is a test exception")
+#     except Exception as e:
+#      error_instance = CustomException(e, sys)
+#      logging.error(error_instance.error_message)
